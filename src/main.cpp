242e66496a9e583caac2ef6de39e22dafa873da6
thigoai <tfreire352@gmail.com>
2026-04-05 21:59:17 -0300

    add codigo inicial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define TRIG 5
+#define ECHO 18
+#define SERVO_PIN 13
+
+Servo servo;
+
+float readDistance(){
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+
+  float duration = pulseIn(ECHO, HIGH);
+  float distance = duration * 0.034 / 2;
+
+  return distance;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // indo
+  for (int angle = 0; angle <=150; angle++){
+   servo.write(angle);
+   delay(5);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+   float dist = readDistance();
+
+   Serial.print(angle);
+   Serial.print(" / ");
+   Serial.println(dist);
+  }
+
+  // voltando
+  for (int angle = 150; angle >= 0; angle--) {
+    servo.write(angle);
+    delay(5);
+
+    long dist = readDistance();
+
+    Serial.print(angle);
+    Serial.print(",");
+    Serial.println(dist);
+  }
 }
